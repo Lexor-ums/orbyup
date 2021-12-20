aed9a89bd0c60ba4483339fdb907339fa9ca72b8
Lexor <su.lexxor@gmail.com>
2021-12-21 01:17:43 +0400

    add uml

---
+++ src/main/java/com/example/orbyup/OrbyupApplication.java
@@ -0,0 +1,13 @@
+package com.example.orbyup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbyupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrbyupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/orbyup/OrbyupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orbyup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbyupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
